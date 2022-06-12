bbce0b4793198d45559ef891cd5282747f0f4742
Thibault Vinchent <tvinchent@gmail.com>
2022-06-12 18:15:25 +0200

    with blog post

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,19 +1,46 @@
 <?php
 
 namespace App\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Post;
+use Doctrine\Persistence\ManagerRegistry;
+
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index() : Response
+
+    #[Route('/hello/{name}', name: 'hello_name')]
+    public function name($name) : Response
     {
-        return new Response ('hello');
+        return new Response ('Hello ' . $name . '!');
         // return $this->render('home/index.html.twig', [
         //     'controller_name' => 'HomeController',
         // ]);
+    }
+
+    #[Route('/', name: 'blog')]
+    public function index(ManagerRegistry $doctrine) : Response
+    {
+        $posts = $doctrine->getRepository(Post::class)->findAll();
+
+        return $this->render('home/index.html.twig', [
+            'title' => 'A simple blog, but with symfo 6!',
+            'posts' => $posts
+        ]);
+    }
+
+    #[Route('/post/{id}', name: 'blogPost')]
+    public function post(ManagerRegistry $doctrine, int $id) : Response
+    {
+        $post = $doctrine->getRepository(Post::class)->find($id);
+
+        return $this->render('home/post.html.twig', [
+            'title' => 'Detail of a post',
+            'post' => $post
+        ]);
     }
 }
 
